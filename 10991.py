67db7736afe6e9e06c59ed05bb48352bafa81501
Chris <hereokay88@gmail.com>
2023-02-13 13:08:33 +0900

    0213-1

---
+++ 10991.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(1,n+1):
+    for j in range(1,2*n):
+        if ( j <= (i+n-1)) and ( (i+j) >= n+1 ) and (((i+j)%2)!=(n%2)):
+            print('*', end='')
+        elif (j <= (i+n-1)):
+            print(' ', end='')
+    if i != n:
+        print()
